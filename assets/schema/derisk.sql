fd312d303fd9161ed4a873d9a29f259e419ad5ff
nishenghao.nsh <nishenghao.nsh@oceanbase.com>
2026-03-17 00:32:17 +0800

    static build uopdate

--- assets/schema/derisk.sql
+++ assets/schema/derisk.sql
@@ -9,7 +9,7 @@ use derisk;
 -- MySQL DDL Script for Derisk
 -- Version: 0.3.0
 -- Generated from SQLAlchemy ORM Models
--- Generated: 2026-03-16 15:59:52
+-- Generated: 2026-03-17 00:25:39
 -- ============================================================
 
 SET NAMES utf8mb4;
