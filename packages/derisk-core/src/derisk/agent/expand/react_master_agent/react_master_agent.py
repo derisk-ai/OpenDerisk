d0e5ab523f5c6379a4aeafa053a0024a6bb0328a
yhjun1026 <460342015@qq.com>
2026-03-30 16:06:40 +0800

    feat: default init agent optimze

--- packages/derisk-core/src/derisk/agent/expand/react_master_agent/react_master_agent.py
+++ packages/derisk-core/src/derisk/agent/expand/react_master_agent/react_master_agent.py
@@ -148,9 +148,9 @@ class ReActMasterAgent(ConversableAgent):
 
     profile: ProfileConfig = Field(
         default_factory=lambda: ProfileConfig(
-            name="ReActMasterV2",
-            role="ReActMasterV2",
-            goal="一个遵循最佳实践的 ReAct 代理，通过系统化推理和工具使用高效解决复杂任务。",
+            name="BAIZE",
+            role="BAIZE",
+            goal="白泽，一个遵循最佳ReAct推理范式实践的Agent，通过系统化推理和工具使用高效解决复杂任务。",
             system_prompt_template=None,
             user_prompt_template=None,
             write_memory_template=REACT_MASTER_FC_WRITE_MEMORY_TEMPLATE_CN,
