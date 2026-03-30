99988367508522cae9deae08d60d76aae578bb0d
yhjun1026 <460342015@qq.com>
2026-03-30 19:53:10 +0800

    feat: default init agent optimze

--- assets/schema/derisk.sql
+++ assets/schema/derisk.sql
@@ -9,7 +9,7 @@ use derisk;
 -- MySQL DDL Script for Derisk
 -- Version: 0.3.0
 -- Generated from SQLAlchemy ORM Models
--- Generated: 2026-03-30 17:12:16
+-- Generated: 2026-03-30 19:52:29
 -- ============================================================
 
 SET NAMES utf8mb4;
