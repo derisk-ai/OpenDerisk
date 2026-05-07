7e0e37401d9b35fd9077c9981f9d0e3d889f4f48
yhjun1026 <460342015@qq.com>
2026-05-06 17:02:39 -0700

    前端bug

--- assets/schema/derisk.sql
+++ assets/schema/derisk.sql
@@ -9,7 +9,7 @@ use derisk;
 -- MySQL DDL Script for Derisk
 -- Version: 0.3.0
 -- Generated from SQLAlchemy ORM Models
--- Generated: 2026-05-06 08:13:53
+-- Generated: 2026-05-06 17:02:22
 -- ============================================================
 
 SET NAMES utf8mb4;
